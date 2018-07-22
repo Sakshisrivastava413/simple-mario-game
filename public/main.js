9cd743e6269277175baf43fcdc2ed78e21fe5c7d
Sakshi <sakshisrivastava413@gmail.com>
2018-07-22 11:40:41 +0530

    loaded images

--- public/main.js
+++ public/main.js
@@ -4,9 +4,14 @@ var gameObject = {
 
   preload: function () {
 
+    game.load.image('background', './assets/background.jpg');
+    game.load.spritesheet('tiles', './assets/tiles.png')
   },
 
   create: function () {
+    
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.add.sprite(0, 0, 'background');
 
   },
 
