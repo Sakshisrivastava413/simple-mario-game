b029b3ebad5c9d28564b852317c1527cd6a28f30
Sakshi <sakshisrivastava413@gmail.com>
2018-07-22 12:30:23 +0530

    added tiles

--- public/main.js
+++ public/main.js
@@ -1,19 +1,36 @@
 var game = new Phaser.Game(1275, 650, Phaser.CANVAS);
 
+var platforms;
+
 var gameObject = {
 
   preload: function () {
 
     game.load.image('background', './assets/download.jpeg');
-    game.load.spritesheet('tiles', './assets/tiles.png')
+    game.load.spritesheet('tiles', './assets/brick.jpg')
   },
 
   create: function () {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     var back = game.add.sprite(0, 0, 'background');
-    back.width = 1275
-    back.height = 650
+    back.width = 1275;
+    back.height = 650;
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    var ground = platforms.create(0, game.world.height - 47, 'tiles');
+    ground.body.immovable = true;
+
+    var ledge = platforms.create(520, 400, 'tiles');
+        ledge.body.immovable = true;
+        ledge = platforms.create(-150, 250, 'tiles');
+        ledge.body.immovable = true;
+        ledge= platforms.create(1000, game.world.height - 47, 'tiles');
+        ledge.body.immovable = true;
+        
+
   },
 
   update: function () {
