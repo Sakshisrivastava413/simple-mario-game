3c129a65e94cc56b350181f2b12f27d51340ac73
Sakshi <sakshisrivastava413@gmail.com>
2018-07-22 13:20:06 +0530

    added player with bounce and gravity effect

--- public/main.js
+++ public/main.js
@@ -7,7 +7,8 @@ var gameObject = {
   preload: function () {
 
     game.load.image('background', './assets/download.jpeg');
-    game.load.spritesheet('tiles', './assets/brick.jpg')
+    game.load.spritesheet('tiles', './assets/brick.jpg');
+    game.load.spritesheet('player', './assets/dude.png', 32, 48);
   },
 
   create: function () {
@@ -20,16 +21,28 @@ var gameObject = {
     platforms = game.add.group();
     platforms.enableBody = true;
 
+
     var ground = platforms.create(0, game.world.height - 47, 'tiles');
     ground.body.immovable = true;
 
     var ledge = platforms.create(520, 400, 'tiles');
-        ledge.body.immovable = true;
-        ledge = platforms.create(-150, 250, 'tiles');
-        ledge.body.immovable = true;
-        ledge= platforms.create(1000, game.world.height - 47, 'tiles');
-        ledge.body.immovable = true;
-        
+    ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'tiles');
+    ledge.body.immovable = true;
+    ledge = platforms.create(1000, game.world.height - 47, 'tiles');
+    ledge.body.immovable = true;
+    // ledge.scale.setTo(-0.9, -0.7);
+    
+
+    player = game.add.sprite(32, game.world.height - 250, 'player');
+    player.scale.setTo(2, 2.5);
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.frame = 4
 
   },
 
